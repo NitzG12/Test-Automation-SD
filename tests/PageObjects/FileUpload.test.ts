400fd991a3ea88d100d19478eb01c1034bde3a66
NitzG12 <nitishg714@gmail.com>
2025-03-24 14:45:59 +0400

    modified FileUpload.test.ts

--- tests/PageObjects/FileUpload.test.ts
+++ tests/PageObjects/FileUpload.test.ts
@@ -1,14 +1,15 @@
-
 import { test } from '@playwright/test';
+
 test('Upload', async ({ page }) => {
-    
     await page.goto('https://globalsolutions-reference.gpi-test.globepayroll.net/ui/#/ess/personal-info');
 
-   const uploadINput = await page.locator(input _ngcontent-njq-c77 type="file");
+    // Correct locator using the full XPath
+    const uploadInput = await page.locator('//html/body/app/div/div[2]/div[2]/ess-personal-info/gp-contract-data/gp-modal/div/div/div/div[2]/form/gp-form-card-field[2]/div/div/gp-form-field-file/label/input');
+    //const uploadInput = await page.locator('input[test-id="CertificationScannedCopy"]');
 
-    await uploadINput.setInputFiles('File1.pdf');
+  //  Upload a file
+    await uploadInput.setInputFiles(['testFile.txt'],{timeout: 15000});
 
-
-    await uploadINput.setInputFiles([]);
-  
+    // // Clear the file input
+    // await uploadInput.setInputFiles([]);
 });
